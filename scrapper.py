fdcad85c3f19d59b2526f4a3d8b3bea7d3eefec6
Louve Le Bronec <lebroneclo@cy-tech.fr>
2022-11-16 15:25:52 +0100

    premier commit

---
+++ scrapper.py
@@ -0,0 +1,107 @@
+#import packages
+import requests
+from bs4 import BeautifulSoup
+import re  
+import json as json  
+from unidecode import unidecode
+import sys, getopt
+
+
+dict = {}
+
+def readFile(inputFile):
+    # Opening JSON file
+    f = open(inputFile)
+    
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+    return data
+
+def writeFile(outputFile, dict):
+    # Write the initial json object (list of dicts)
+    with open(outputFile, mode='w',encoding='utf8') as f:
+        json.dump(dict, f, ensure_ascii=False)
+
+def extract_video_informations(url, inputFile, outputFile):  
+    
+    # importer le code de la page
+    response=requests.get(url)
+    soup=BeautifulSoup(response.text,"html.parser")
+
+    result = {}  
+
+    result["title"] = soup.find("meta", itemprop="name")['content']  
+    result["VideoMaker"] = soup.find("span", itemprop="author").next.next['content'] 
+
+    data = re.search(r"var ytInitialData = ({.*?});", soup.prettify()).group(1)  
+    data_json = json.loads(data) 
+
+    filename = "tmp.json"
+    # Write the initial json object (list of dicts)
+    with open(filename, mode='w') as f:
+        json.dump(data_json, f)
+
+
+    videoPrimaryInfoRenderer = data_json['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']  
+
+     # number of likes  
+    likes_label = videoPrimaryInfoRenderer['videoActions']['menuRenderer']['topLevelButtons'][0]['segmentedLikeDislikeButtonRenderer']['likeButton']['toggleButtonRenderer']['defaultText']['accessibility']['accessibilityData']['label']# "No likes" or "###,### likes"  
+
+    likes_str = likes_label.split(' ')[0].replace(',','')  
+    result["likes"] = '0' if likes_str == 'No' else unidecode(likes_str)  
+
+    dict_tmp = data_json['contents']["twoColumnWatchNextResults"]["results"]["results"]["contents"][1]["videoSecondaryInfoRenderer"]["description"]["runs"]
+
+    res = ''
+    list_link = []
+    for i in range(len(dict_tmp)):
+
+        if 'text' in dict_tmp[i].keys():
+
+            res += dict_tmp[i]['text']
+            
+            if "http" in dict_tmp[i]['text']:
+                list_link.append(dict_tmp[i]['text'])
+    result["description"] = res
+    result["links"] = list_link
+    result["id"] = soup.find("meta", itemprop="videoId")['content'] 
+
+    return(result)
+
+
+
+
+def readArguments(argv):
+    inputFile = ''
+    outputFile = ''
+    try:
+        options, args = getopt.getopt(argv,"hi:o:",["input=","output="])
+    except getopt.GetoptError:
+        print ('scrapper.py --input <inputfile> --output <outputfile>')
+        sys.exit(2)
+    for opt, arg in options:
+        if opt == '-h':
+            print ('scrapper.py --input <inputfile> --output <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputFile = arg
+        elif opt in ("-o", "--output"):
+            outputFile = arg
+    print ('Input file is :', inputFile)
+    print ('Output file is :', outputFile)
+    return inputFile, outputFile
+
+if __name__ == "__main__":
+    inputFile, outputFile =  readArguments(sys.argv[1:])
+
+    data = readFile(inputFile)
+
+    # Iterating through the json
+    # list
+    for id in data['videos_id']:
+        dict_result = extract_video_informations("https://www.youtube.com/watch?v="+id, inputFile, outputFile)
+        dict[id] = dict_result
+
+    writeFile(outputFile, dict)
+   
